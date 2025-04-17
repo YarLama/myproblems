72f95582c8a4c9220f10faeee39d1efbe5883ce9
YarLama <gaideichuk1998@mail.ru>
2025-04-17 16:59:05 +0300

    #0.6. test commit #2

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="flex flex-col">
       <h1>Test Icon</h1>
+      <h1>Test #2</h1>
       <p>Test message</p>
       <IconButton icon="delete"/>
       <span
