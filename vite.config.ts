af086e9d7b746928f625038e84e56cd3216b4dac
YarLama <yarlamas@gmail.com>
2026-05-12 13:48:34 +0300

    wip: change config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcssvite()],
-    base: "/myproblems/",
+    base: "/myproblems",
     build: {
       sourcemap: false,
     },
