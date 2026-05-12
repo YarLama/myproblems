ec3415da9212d543078b90f303b4ec0c4adb90c3
YarLama <yarlamas@gmail.com>
2026-05-12 13:34:48 +0300

    wip: change config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcssvite from "@tailwindcss/vite";
 import path from "path";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
+export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcssvite()],
     base: "/myproblems/",
